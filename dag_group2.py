79fb37ef8b76b3904f61fc779b25d36690b32123
stephane hollander <hollanderstephane5@gmail.com>
2020-12-09 14:43:49 +0100

    update dag

--- dag_group2.py
+++ dag_group2.py
@@ -10,21 +10,35 @@ default_dag_args = {
 dag = DAG(
     dag_id='group2_dag',
     schedule_interval = timedelta(days=1),
+    retry_delay = timedelta(minutes=1),
     default_args=default_dag_args)
 
-task1 = BashOperator(
-task_id = "task1",
-bash_command = "echo hello world 1",
-dag = dag)  
+#extract_data = PythonOperator(
+#    task_id = "extract_data",
+#    bash_command = "echo python3 download-data.py",
+#    dag = dag
+#)  
 
-task2 = BashOperator(
-task_id = "task2",
-bash_command = "echo hello world 2",
-dag = dag) 
+#spark_submit = BashOperator(
+#    task_id = "spark_submit",
+#    bash_command = "echo spark-submit --deploy-mode cluster --master yarn --class autom.main spark.jar",
+#    dag = dag
+#)
 
 task3 = BashOperator(
-task_id = "task3",
-bash_command = "echo hello world 3",
-dag = dag)
+    task_id = "task3",
+    bash_command = "echo hello world 3",
+    dag = dag
+)
+task2 = BashOperator(
+    task_id = "task2",
+    bash_command = "echo hello world 2",
+    dag = dag
+)
+task1 = BashOperator(
+    task_id = "task1",
+    bash_command = "echo hello world 1",
+    dag = dag
+)
 
 task1 >> task2 >> task3
