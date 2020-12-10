c55609e1dd057920c7ef892e498b08de3a208c73
stephaneArtist <hollanderstephane5@gmail.com>
2020-12-10 12:13:50 +0100

    update dag

--- dag_group2.py
+++ dag_group2.py
@@ -9,20 +9,18 @@ from datetime import datetime, timedelta
 default_dag_args = {
     'owner': 'group2',
     'start_date': datetime.now(),
-    'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
-dag = DAG(
+with DAG(
     dag_id='group2_dag',
-    schedule_interval = timedelta(days=1),
+    schedule_interval = timedelta(minutes=1),
     default_args=default_dag_args
-)
-
-task1 = BashOperator(
-    task_id = "task1",
-    bash_command = "echo hello world",
-    dag = dag
-)
+) as dag:
+operators
+    task1 = BashOperator(
+        task_id = "task1",
+        bash_command = "echo hello world"
+    )
 
 """ extract_data = BashOperator(
     task_id = "extract_data",
@@ -30,11 +28,10 @@ task1 = BashOperator(
     dag = dag
 )     """
 
-spark_submit = BashOperator(
-    task_id = "spark_submit",
-    bash_command = "spark-submit --deploy-mode cluster --master yarn --class job.stat hdfs:///user/iabd2_group2/Stat.jar",
-    dag = dag
-)
+    spark_submit = BashOperator(
+        task_id = "spark_submit",
+        bash_command = "spark-submit --deploy-mode cluster --master yarn --class job.stat hdfs:///user/iabd2_group2/Stat.jar"
+    )
 
 """ extract_data = PythonOperator(
     task_id = "extract_data",
@@ -55,4 +52,4 @@ task2 = BashOperator(
 
  """
 #task1 >> task2 >> task3
-task1 >> spark_submit
+    task1 >> spark_submit
