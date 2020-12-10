aae874aacf6d992ced034dc965f995a06e0732c0
stephaneArtist <hollanderstephane5@gmail.com>
2020-12-10 01:23:23 +0100

    update dag

--- dag_group2.py
+++ dag_group2.py
@@ -23,23 +23,17 @@ extract_data = BashOperator(
     bash_command = "echo test",
     dag = dag
 )  
-
-extract_data = BashOperator(
-    task_id = "extract_data",
-    bash_command = "ifconfig",
-    dag = dag
-)  
 """ extract_data = PythonOperator(
     task_id = "extract_data",
     python_callable = main,
     dag = dag
-) 
+)  """ 
 
 spark_submit = BashOperator(
     task_id = "spark_submit",
     bash_command = "spark-submit --deploy-mode cluster --master yarn --class job.stat Stats.jar",
     dag = dag
-) """
+)
 
 """ task3 = BashOperator(
     task_id = "task3",
